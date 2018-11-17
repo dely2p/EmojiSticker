e3d87a5a607764efa546e9ca4ffabeb7450d4050
dely2p <lhyal2p@gmail.com>
2018-11-17 16:38:12 +0900

    add code to set URLSession to use clova face API

--- EmojiSticker/EmojiSticker/ViewController.swift
+++ EmojiSticker/EmojiSticker/ViewController.swift
@@ -9,13 +9,114 @@
 import UIKit
 import Vision
 
+struct FaceInfo {
+    let size : (width: Int, height: Int)
+    let faceCount : Int
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // set imageView
+        guard let image = UIImage(named: "sample1") else {
+            return
+        }
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFit
         
+        let scaledHeight = view.frame.width / image.size.width * image.size.height
+        
+        imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: scaledHeight)
+        imageView.backgroundColor = .blue
+        
+        view.addSubview(imageView)
+        
+        
+        // set URLSession
+        guard let url = URL(string: "https://openapi.naver.com/v1/vision/face") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        let boundary = generateBoundaryString()
+        request.addValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        request.addValue("mBMpvpj5zqP6d0eidQM4", forHTTPHeaderField: "X-Naver-Client-Id")
+        request.addValue("NRhG38_jQo", forHTTPHeaderField: "X-Naver-Client-Secret")
+        request.addValue("96703", forHTTPHeaderField: "Content-Length")
+        
+        guard let imageData = imageView.image?.jpegData(compressionQuality: 1) else { return }
+        
+        let param: [String:String]? = nil
+        
+        request.httpBody = createBodyWithParameters(parameters: param, filePathKey: "image", imageDataKey: imageData, boundary: boundary) as Data
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data, error == nil else { return }
+            
+            do {
+                guard let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
+                    return
+                }
+                let faceInfo = FaceInfo(json: json)
+                print(faceInfo?.faceCount)
+            } catch let jsonErr{
+                print("Error ", jsonErr)
+            }
+        }
+        task.resume()
+        
+    }
+    
+    func createBodyWithParameters(parameters: [String: String]?, filePathKey: String?, imageDataKey: Data, boundary: String) -> NSData {
+        let body = NSMutableData();
+        
+        if parameters != nil {
+            for (key, value) in parameters! {
+                body.appendString("--\(boundary)\r\n")
+                body.appendString("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n")
+                body.appendString("\(value)\r\n")
+            }
+        }
+        
+        let filename = "sample1.jpg"
+        let mimetype = "image/jpg"
+        
+        body.appendString("--\(boundary)\r\n")
+        body.appendString("Content-Disposition: form-data; name=\"\(filePathKey!)\"; filename=\"\(filename)\"\r\n")
+        body.appendString("Content-Type: \(mimetype)\r\n\r\n")
+        body.append(imageDataKey as Data)
+        body.appendString("\r\n")
+        body.appendString("--\(boundary)--\r\n")
+        
+        return body
+    }
+    
+    func generateBoundaryString() -> String {
+        return "Boundary-\(UUID().uuidString)"
     }
 
 }
 
+extension NSMutableData {
+    
+    func appendString(_ string: String) {
+        let data = string.data(using: String.Encoding.utf8, allowLossyConversion: true)
+        append(data!)
+    }
+}
+
+extension FaceInfo {
+    init?(json: [String:Any]) {
+        guard let faceCount = json["faceCount"] as? Int,
+            let size = json["size"] as? [String:Int],
+            let width = size["width"],
+            let height = size["height"]
+        else {
+            return nil
+        }
+        self.faceCount = faceCount
+        self.size = (width, height)
+    }
+}
